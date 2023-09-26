c18082a98d0a0cb5bd0888a7e5b7c0b95bcb1191
pengjia <pengjia1110@gmail.com>
2023-09-26 21:22:32 +0800

    first

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,22 @@ release = 'v0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+'myst_parser',
+    "sphinxcontrib.mermaid",
+    "sphinx_copybutton",
+]
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
+
+myst_enable_extensions = [
+    "tasklist",
+    "deflist",
+    "dollarmath",
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +39,20 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+    'analytics_anonymize_ip': False,
+    'logo_only': True,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False,
+}
 html_static_path = ['_static']
+# html_js_files = [
+#     'my_custom.js',
+# ]
